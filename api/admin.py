1691ac67b35c40840547778c7e5cb5e2ec82d2a9
nnsnodnb <ahr63_gej@me.com>
2016-11-19 00:04:54 +0900

    Visualization ReceiveSentence models in admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ReceiveSentence
 
-# Register your models here.
+class ReceiveSentenceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sentence', 'receive_date')
+    list_display_links = ('id', 'sentence', 'receive_date')
+
+admin.site.register(ReceiveSentence, ReceiveSentenceAdmin)
