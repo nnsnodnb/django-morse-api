925e7e33e4fd7e20bd3bdbd91e1b478b6cf29d6c
nnsnodnb <ahr63_gej@me.com>
2016-12-23 14:38:00 +0900

    Add Static root

--- morse_api/settings.py
+++ morse_api/settings.py
@@ -136,3 +136,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
